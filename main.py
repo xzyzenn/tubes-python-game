012c93c373fb8e48a09d62168061e51965aa94ab
George Haansraj <georgehaansraj@gmai.com>
2025-05-12 20:18:55 +0700

    bikin game window dan buat folder-file penting

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Game dimulai!")
+import pygame as pg
+import constants as c
+
+#insialisasi pygame
+pg.init()
+
+#buat waktu
+clock = pg.time.Clock()
+
+#buat game window
+screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+pg.display.set_caption ("Tower Defense")
+
+#game loop
+run = True
+while run:
+  
+  clock.tick(c.FPS)
+
+  #event handler
+  for event in pg.event.get():
+    #keluar program
+    if event.type == pg.QUIT:
+      run = False
+
+pg.quit()
